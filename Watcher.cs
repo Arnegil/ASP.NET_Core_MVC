0a5e417bde84817824e33253430527607b04bc15
Arnegil <i.arnegil@gmail.com>
2017-10-22 09:40:19 +0500

    Fix

---
+++ Watcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Middleware
+{
+    public class Watcher
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+
+        public long Milliseconds => _endTime.Subtract(_startTime).Milliseconds;
+
+        public void Start()
+        {
+            _startTime = DateTime.Now;
+        }
+
+        public void End()
+        {
+            _endTime = DateTime.Now;
+        }
+    }
+}

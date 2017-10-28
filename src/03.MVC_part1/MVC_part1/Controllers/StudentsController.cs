9e791b04d0e01b1c52fada24fc115ced82b15cd3
Arnegil <i.arnegil@gmail.com>
2017-10-28 20:21:50 +0500

    MVC part 1

---
+++ src/03.MVC_part1/MVC_part1/Controllers/StudentsController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC_part1.Models;
+
+namespace MVC_part1.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private static readonly List<Student> Students = new List<Student>
+        {
+            new Student
+            {
+                Id = 0,
+                FirstName = "Петр",
+                MiddleName = "Петрович",
+                LastName = "Петров",
+                City = "Екатеринбург",
+                BirthDate = new DateTime(1990, 04, 23),
+                TabelNumber = 4927592
+            }
+        };
+        
+        public IActionResult Index()
+        {
+            return View(new StudentsModel{Students = Students});
+        }
+        
+        public Student CreateStudent(Student student)
+        {
+            if (Students.Any(x => x.Id == student.Id))
+                throw new Exception($"Already exists with id = {student.Id}");
+            Students.Add(student);
+            return student;
+        }
+        
+        public Student GetStudent(int id)
+        {
+            if (!Students.Exists(x => x.Id == id))
+                throw new Exception($"Not exists with id = {id}");
+            return Students.Find(x => x.Id == id);
+        }
+
+        public Student UpdateStudent(Student student)
+        {
+            if (!Students.Exists(x => x.Id == student.Id))
+                throw new Exception($"Not exists with id = {student.Id}");
+            Students.RemoveAll(x => x.Id == student.Id);
+            Students.Add(student);
+            return student;
+        }
+
+        public string DeleteStudent(int id)
+        {
+            if (!Students.Exists(x => x.Id == id))
+                throw new Exception($"Not exists with id = {id}");
+            Students.RemoveAll(x => x.Id == id);
+            return "Success delete!";
+        }
+    }
+}
---
+++ src/03.MVC_part1/MVC_part1/Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_part1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string City { get; set; }
+        public int TabelNumber { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(LastName) ||
+                    string.IsNullOrEmpty(FirstName) ||
+                    string.IsNullOrEmpty(MiddleName))
+                    return null;
+
+                return $"{LastName} {FirstName[0]}. {MiddleName[0]}.";
+            }
+        }
+}
+}
